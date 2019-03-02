4a6eda91a6185eb6b8d687a1c59ba7fce5a84e29
Dan Thareja <danthareja@gmail.com>
2019-03-02 13:31:34 -0700

    Add notifications

--- src/components/Board/Notification.js
+++ src/components/Board/Notification.js
@@ -1,15 +1,48 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import './Notification.css';
+import './Notification.css'
 
-const Notification = function({ tile }) {
-  return (
-    <div className="Notification">Notification</div>
-  )
-};
+class Notification extends Component {
+  constructor(props) {
+    super(props);
 
-Notification.propTypes = {
-  tile: PropTypes.any.isRequired,
-};
+    if (props.onDismiss && props.isActive) {
+      this.dismissTimeout = setTimeout(
+        props.onDismiss,
+        props.dismissAfter
+      );
+    }
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.dismissAfter === false) return;
+
+    if (nextProps.onDismiss) {
+      if (
+        (nextProps.isActive && !this.props.isActive) ||
+        (nextProps.dismissAfter && this.props.dismissAfter === false)
+      ) {
+        this.dismissTimeout = setTimeout(
+          nextProps.onDismiss,
+          nextProps.dismissAfter
+        );
+      }
+    }
+  }
+
+  componentWillUnmount() {
+    if (this.props.dismissAfter) clearTimeout(this.dismissTimeout);
+  }
+
+  render() {
+    return (
+      <div className={`Notification ${this.props.isActive ? 'Notification--fadeIn' : 'Notification--fadeOut'}`}>
+        <span>
+          {this.props.message}
+        </span>
+      </div>
+    );
+  }
+}
 
 export default Notification;
